bfda1905736c5480959ab5e681448a8710436cbf
bahaa911 <71220632+bahaa911@users.noreply.github.com>
2023-11-08 11:14:41 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,9 @@
 SMOKE_RESULTS="result.csv"
 LOG_FILE="log.log"
-	JMETER_SCRIPT_FILE=$1
- 
-SCRIPT_NAME=${JMETER_SCRIPT_FILE%%.*}
- 
-jmeter -n -t $JMETER_SCRIPT_FILE \
-	-l /data/$SMOKE_RESULTS -j /data/$LOG_FILE
+JMETER_SCRIPT_FILE=$1
+jmeter -n -t /data/$JMETER_SCRIPT_FILE \
+    -l /data/$SMOKE_RESULTS -j /data/$LOG_FILE
 
 echo "--------------- execution done --------------"
 
-#cat $SMOKE_RESULTS
-
 echo "--------------- check Logs below --------------"
-
-#cat $LOG_FILE
